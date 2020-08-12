3b4d38179774aa044a5b883bbc8eea62e96f16fd
Sam Snarr <sssnarr1@gmail.com>
2020-08-12 19:47:53 -0400

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { TimeInterval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-electron-timer';
+  startTime: number;
+  elapsedTime: number;
+  timerLength: number;
+  delta: 100; // milliseconds
+  interval: any;
+
+  start(): void {
+    this.startTime = Date.now();
+
+    this.timerLength = this.timerLength * 1000 * 60;
+
+    this.interval = setInterval(() => {
+      this.elapsedTime = Date.now() - this.startTime;
+      console.log(
+        this.elapsedTime,
+        this.timerLength,
+        (100 * (this.timerLength - this.elapsedTime)) / this.timerLength
+      );
+
+      if (this.elapsedTime >= this.timerLength) {
+        clearInterval(this.interval);
+      }
+    }, this.delta);
+  }
+
+  stop(): void {
+    // this.elapsedTime = this.timerLength;
+    clearInterval(this.interval);
+  }
+
+  changeInput(event: any): void {
+    this.timerLength = event.target.value;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgCircleProgressModule.forRoot({
+      radius: 60,
+      outerStrokeWidth: 10,
+      innerStrokeWidth: 5,
+      showBackground: false,
+      startFromZero: false,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
