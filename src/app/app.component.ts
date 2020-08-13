aa31c4766ab93f64b44f0ea0914919cff8100ba3
Sam Snarr <sssnarr1@gmail.com>
2020-08-13 14:37:06 -0400

    refactored logic for spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TimeInterval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,20 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   startTime: number;
-  elapsedTime: number;
-  timerLength: number;
-  delta: 100; // milliseconds
-  interval: any;
+  elapsedTime = 0;
+  delta = 1000; // milliseconds
+  interval: number;
+  timeLength: number;
+
+  inputValue = 0; // number of minutes
+  isStarted = false;
 
   start(): void {
+    this.isStarted = true;
     this.startTime = Date.now();
-
-    this.timerLength = this.timerLength * 1000 * 60;
 
     this.interval = setInterval(() => {
       this.elapsedTime = Date.now() - this.startTime;
 
-      if (this.elapsedTime >= this.timerLength) {
+      if (this.elapsedTime >= this.inputValue) {
         this.stop();
         this.playChime();
       }
@@ -33,12 +36,29 @@ export class AppComponent {
     audio.play();
   }
 
+  pause(): void {
+    this.stop();
+    this.inputValue = (this.inputValue - this.elapsedTime) / 60 / 1000;
+  }
+
   stop(): void {
     // this.elapsedTime = this.timerLength;
+    this.isStarted = false;
     clearInterval(this.interval);
   }
 
-  changeInput(event: any): void {
-    this.timerLength = event.target.value;
+  msToTime(s): string {
+    const ms = s % 1000;
+    s = (s - ms) / 1000;
+    const secs = s % 60;
+    s = (s - secs) / 60;
+    const mins = s % 60;
+    const hrs = (s - mins) / 60;
+
+    return hrs + ':' + mins + ':' + secs;
   }
+
+  // changeInput(event: any): void {
+  //   this.timerLength = event.target.value;
+  // }
 }
